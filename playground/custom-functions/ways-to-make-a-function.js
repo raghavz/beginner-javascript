0d796c4f7f97d54ff5eb5857e650ac632eb8f24a
raghavz <raghavz@email.com>
2020-08-11 14:16:20 -0700

    function expression demo.

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -10,3 +10,12 @@ function doctorize(firstName) {
   return `Dr. ${firstName}`;
 }
 // console.log(`Function declaration 2 : ${doctorize('James')} `); // Function call after function declaration - It works !
+
+// FN_2 : With function expression.
+/* Note : These are not hoisted ("refer - function hoisting"). Hence, you can't use such functions before they are declared. */
+
+// doctorizeFn('John'); // It fails ! Uncaught ReferenceError: Cannot access 'doctorizeFn' before initialization.
+const doctorizeFn = function(firstName) {
+  return `Dr. ${firstName}`;
+};
+// doctorizeFn('John'); // It works !
