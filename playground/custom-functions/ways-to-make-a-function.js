3c7fee3de6fb230a587bca6f9281dcf1ba44ebd8
raghavz <raghavz@email.com>
2020-08-11 12:44:33 -0700

    commented the previous check.

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,1 +1,2 @@
-console.log('This js file works!'); // should appear in browser console (browser can be opened via Live Server plugin for VS code).
+// The message should appear in browser console (browser can be opened via Live Server plugin for VS code).
+// console.log('This js file works!'); //Commented because its only needed to test if index.html can load this js file.
