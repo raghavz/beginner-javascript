9289cdedf0b31bcb856bec95930c982b904d6968
raghavz <raghavz@email.com>
2020-08-11 14:13:31 -0700

    function declaration demo.

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,2 +1,12 @@
 // The message should appear in browser console (browser can be opened via Live Server plugin for VS code).
-// console.log('This js file works!'); //Commented because its only needed to test if index.html can load this js file.
+console.log('This js file works!'); // Open index.html to check if it can run this js script.
+
+// ***Ways to make a function :***
+
+// FN_1 : With function declaration.
+
+// console.log(`Function declaration 1 : ${doctorize('James')} `); // Function call before function declaration - It works !
+function doctorize(firstName) {
+  return `Dr. ${firstName}`;
+}
+// console.log(`Function declaration 2 : ${doctorize('James')} `); // Function call after function declaration - It works !
