8d481634acfbf96d0ac2b923cc0a0f9af95ae7c6
raghavz <raghavz@email.com>
2020-08-12 09:30:32 -0700

    added examples of callback functions.

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -28,9 +28,33 @@ expressions. Read further */
   return `You are cool and age ${age}`;
 })(10); // The (10) at the end shows that we call this function (just after declaring it).
 
-// FN 5 : Callback functions - TODO.
-function greeter1(name, c) {
-  console.log('Great Clicking!!');
+// FN_5 : Callback functions.
+/* We'll first show some silly examples for callback functions. After that, we show a more realistic example. */
+
+function greeter1(name, callbackFunction) {
+  console.log(`${callbackFunction()} ${name}`);
 }
+
+function sayBye() {
+  return 'Goodbye';
+}
+
+const sayHello = function() {
+  return 'Hello';
+};
+
+greeter1('John', sayHello); // Callback using function expression.
+greeter1('Travis', sayBye); // Callback using a named function.
+greeter1('Jane', function() {
+  return 'Nice to meet you';
+}); // Callback using an anonymous function.
+
+/* More realistic example of callback function */
+
+const button = document.querySelector('.clickMe'); // .clickMe is the class of a button in the index.html web page.
+function handleClick() {
+  console.log('You just clicked a button !');
+}
+button.addEventListener('click', handleClick);
 
 // TODO - Next items Arrow functions, Methods. Refer https://www.notion.so/Beginner-JavaScript-e2ef045754d14e96b93791f638bbcaf6.
