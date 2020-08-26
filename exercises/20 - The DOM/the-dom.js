6f5d58b6da21a746b4bcf57ad1b62a7c8ee0ea68
raghavz <raghavz@email.com>
2020-08-25 18:44:48 -0700

    Lesson 22 : The DOM - Element Properties and Methods.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,3 +1,5 @@
+/*eslint-disable*/
+
 /* 
 Where to load JavaScript if we are selecting elements on the page ?
 Best way - Load the JS before the </body> tag.
@@ -8,6 +10,7 @@ Other ways -
  */
 
 // LESSON - The DOM - Selecting elements on the page.
+
 /*
 We can select elements with document.querySelector('aSelector') and document.querySelectorAll('aSelector'). The argument we 
 pass to these functions is nearly similar to CSS Selectors.
@@ -27,3 +30,27 @@ const imgs = document.querySelectorAll('.item img'); // .item refers to differen
 const item2Image = item2.querySelector('img');
 
 // LESSON - The DOM - Element Properties and Methods.
+
+const heading = document.querySelector('h2'); // select the 1st h2 in the dom.
+
+// Console.dir(...) Shows us the properties of an object such as parentElement, textContent, innerText etc.
+console.dir(`console.dir(...) demo :\n${heading}`);
+
+//textContent vs innerText.
+console.log(`textContent demo :\n${heading.textContent}`);
+console.log(`innerText demo :\n${heading.innerText}`);
+
+// Getting & Setting the property of an element.
+const oldHeading = heading.textContent;
+//heading.textContent = 'New text for heading !';
+
+//innerHtml
+console.log(`innerHtml demo :\n${heading.innerHTML}`);
+
+//outerHtml
+console.log(`innerHtml demo :\n${heading.outerHTML}`);
+
+//Adding text at the end of an element : textContent VS insertAdjacentText.
+const pizzaList = document.querySelector('.pizza');
+pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+pizzaList.insertAdjacentText('beforeend', '🍕');
