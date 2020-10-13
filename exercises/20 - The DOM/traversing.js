c2fafa4a47227b4e21f11bbc9c5a8ed72bb66379
raghavz <raghavz@email.com>
2020-10-12 17:28:12 -0700

    completed the code for lesson 27.

--- exercises/20 - The DOM/traversing.js
+++ exercises/20 - The DOM/traversing.js
@@ -1,1 +1,26 @@
 console.log('It works!');
+
+const wes = document.querySelector('.wes');
+
+// These properties return an Element.
+console.log(wes.children);
+console.log(wes.firstElementChild);
+console.log(wes.lastElementChild);
+console.log(wes.previousElementSibling);
+console.log(wes.nextElementSibling);
+console.log(wes.parentElement);
+
+// These properties return a Node.
+console.log(wes.childNodes);
+console.log(wes.firstChild);
+console.log(wes.lastChild);
+console.log(wes.previousSibling);
+console.log(wes.nextSibling);
+console.log(wes.parentNode);
+
+// Removing an element which we added to the DOM.
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.appendChild(p);
+
+p.remove();
