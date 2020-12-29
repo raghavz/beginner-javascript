65d4e9072870a2e209e3d81b1dbf0540d2d7e992
raghavz <raghavz@email.com>
2020-12-28 17:17:57 -0800

    completed my own custom code for lesson 30.

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,7 +1,19 @@
 const buttons = document.querySelector('.buttons');
 
-function handleClick() {
+function handleClick(event) {
   console.log('It got clicked!');
+  console.log(event.target);
+  console.log(event.currentTarget);
 }
 
 buttons.addEventListener('click', handleClick);
+
+/*
+function handleWindowClick(event) {
+  console.log('Window got clicked!');
+  console.log(event.target);
+  console.log(event.currentTarget);
+}
+
+window.addEventListener('click', handleWindowClick, { capture: true });
+*/
