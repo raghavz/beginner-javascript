14782eefa21c334fc578edf86b69249705747075
raghavz <raghavz@email.com>
2021-01-20 22:36:42 -0800

    place the starting dot at a random place on the canvas.

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -4,12 +4,21 @@ const ctx = canvas.getContext('2d'); // ctx is context.
 
 const shakeButton = document.querySelector('.shake');
 
-// setup the canvas for drawing.
+// setup the canvas for drawing:
+
+const { width, height } = canvas; // same as width = canvas.width, followed by height = canvas.height;
+
+// create random x,y coordinates for the starting dot on the canvas.
+const x = Math.floor(Math.random() * width);
+const y = Math.floor(Math.random() * height);
+
+// specify the kind of pen/brush for drawing.
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
-ctx.beginPath(); // start the drawing.
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+// start the drawing.
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
