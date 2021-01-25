bfe10ae4adf4aad05eb732be2523519e97650561
raghavz <raghavz@email.com>
2021-01-25 09:48:48 -0800

    we now get a modal box on clicking buttons.

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,8 +1,13 @@
 // JS part of lesson starts at 8:30.
 
+// Grab the modal box/popup (which is normally hidden.)
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
 function handleCardButtonClick(event) {
   const button = event.currentTarget;
   const card = button.closest('.card'); // look up the DOM to find a card.
+  const name = card.querySelector('h2').textContent;
 
   // Grab the image link.
   const imgSrc = card.querySelector('img').src;
@@ -14,12 +19,18 @@ function handleCardButtonClick(event) {
   const desc = card.dataset.description;
 
   // Add the image and description to the modal box.
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+
+  // Show the modal.
+  modalOuter.classList.add('open');
 }
 
 // Select the buttons of each card.
 const cardButtons = document.querySelectorAll('.card button');
 
-cardButtons.forEach(
-  button => button.addEventListener('click'),
-  handleCardButtonClick
+cardButtons.forEach(button =>
+  button.addEventListener('click', handleCardButtonClick)
 );
